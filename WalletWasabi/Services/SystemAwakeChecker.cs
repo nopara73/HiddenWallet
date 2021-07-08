b9441577e0c3d2d37c04a25c9a5d32ffd7b8ba2b
Kiminuo <kiminuo@protonmail.com>
2021-07-08 12:01:02 +0200

    Implement `SystemdInhibitorTask`.

--- WalletWasabi/Services/SystemAwakeChecker.cs
+++ WalletWasabi/Services/SystemAwakeChecker.cs
@@ -1,14 +1,22 @@
 using System;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using WalletWasabi.Bases;
 using WalletWasabi.Helpers;
+using WalletWasabi.Helpers.PowerSaving;
 using WalletWasabi.Wallets;
+using static WalletWasabi.Helpers.PowerSaving.SystemdInhibitorTask;
 
 namespace WalletWasabi.Services
 {
 	public class SystemAwakeChecker : PeriodicRunner
 	{
+		private const string Reason = "CoinJoin is in progress.";
+		private static readonly TimeSpan Timeout = TimeSpan.FromMinutes(5);
+
+		private volatile IPowerSavingInhibitorTask? _powerSavingTask;
+
 		public SystemAwakeChecker(WalletManager walletManager) : base(TimeSpan.FromMinutes(1))
 		{
 			WalletManager = walletManager;
@@ -18,10 +26,33 @@ namespace WalletWasabi.Services
 
 		protected override async Task ActionAsync(CancellationToken cancel)
 		{
+			IPowerSavingInhibitorTask? task = _powerSavingTask;
+
 			if (WalletManager.AnyCoinJoinInProgress())
 			{
-				await EnvironmentHelpers.ProlongSystemAwakeAsync().ConfigureAwait(false);
+				if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+				{					
+					if (task is not null)
+					{
+						task.Prolong(TimeSpan.FromMinutes(1));
+					}
+					else
+					{
+						_powerSavingTask = SystemdInhibitorTask.Create(InhibitWhat.All, Timeout, Reason);
+					}
+				}
+				else
+				{
+					await EnvironmentHelpers.ProlongSystemAwakeAsync().ConfigureAwait(false);
+				}
 			}
+			else
+			{
+				if (task is not null)
+				{
+					await task.StopAsync().ConfigureAwait(false);
+				}
+			}			
 		}
 	}
 }
